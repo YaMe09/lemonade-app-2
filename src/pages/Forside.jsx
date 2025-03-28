e4474ca557c3a56f4c395d15294a8d0d2fb944d9
YaMe09 <134693503+YaMe09@users.noreply.github.com>
2025-03-28 11:33:41 +0100

    update

--- src/pages/Forside.jsx
+++ src/pages/Forside.jsx
@@ -1,4 +1,4 @@
-import LemonadeLogo from '../assets/lemonadeLogo.png';
+import LemonadeLogo from '../assets/LemonadeLogo.png';
 import 'bulma/css/bulma.min.css';
 import { useNavigate } from 'react-router-dom';
 
